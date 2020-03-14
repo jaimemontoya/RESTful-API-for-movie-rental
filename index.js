f7d1fa01c2d1e5627ae7fe114fbc71cc3234422e
Jaime Montoya <j.montoya@cuponclub.net>
2020-03-13 19:15:00 -0600

    Test running Node and Express server on port 400.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Using ES6 syntax is fine because we already have Babel to transpile code.
+import express from 'express';
+// Create an Express application.
+const app = express();
+// Define port number for the server.
+const PORT = 400;
+// When visitors make a GET request to address "/", specify the response.
+app.get('/', (req, res) =>
+    res.send(`Node and Express server running on port ${PORT}`)
+);
+// Send message to the console to confirm that the server is running on the specified port.
+app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`)
+);
